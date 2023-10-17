1e39c3b11a8fb4f9e9fd158ce86c56e491346fe0
anbehindY <yanminthwin@gmail.com>
2023-10-17 17:58:22 +0630

    Add logout button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,29 @@
+'use client';
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+
 export default () => {
+  const router = useRouter();
+  const logout = () => {
+    try {
+      axios.get('/api/users/logout')
+      router.push('/login')
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  }
   return (
     <div>
       <h1>Profile</h1>
+      <button
+        className="bg-sky-500 p-2"
+        onClick={logout}
+      >
+        Logout
+      </button>
     </div>
   )
 }
