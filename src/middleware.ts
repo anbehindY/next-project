c1afb7ad58630a0055888a87a770aae7eadf3dca
anbehindY <yanminthwin@gmail.com>
2023-10-18 18:55:15 +0630

    Specify public and private pages

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const isPublic = request.nextUrl.pathname === '/login' || request.nextUrl.pathname === '/signup'
+
+  const token = '' || request.cookies.get('token')
+
+  if (isPublic && token) 
+    return NextResponse.redirect(new URL('/profile', request.nextUrl))
+  else if(!isPublic && !token)
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/profile',
+    '/profile/:path*',
+    '/login',
+    '/signup',
+  ]
+}
