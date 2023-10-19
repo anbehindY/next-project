5ccb2d757c4eba09ac584d2dea0c707ae2881f87
anbehindY <yanminthwin@gmail.com>
2023-10-19 10:02:47 +0630

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,22 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const isPublic = request.nextUrl.pathname === '/login' || request.nextUrl.pathname === '/signup'
+	const isPublic =
+		request.nextUrl.pathname === '/login' ||
+		request.nextUrl.pathname === '/signup' ||
+		request.nextUrl.pathname === '/verification';
 
-  const token = '' || request.cookies.get('token')
+	const token = '' || request.cookies.get('token');
 
-  if (isPublic && token) 
-    return NextResponse.redirect(new URL('/profile', request.nextUrl))
-  else if(!isPublic && !token)
-    return NextResponse.redirect(new URL('/login', request.nextUrl))
+	if (isPublic && token)
+		return NextResponse.redirect(new URL('/profile', request.nextUrl));
+	else if (!isPublic && !token)
+		return NextResponse.redirect(new URL('/login', request.nextUrl));
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    '/',
-    '/profile',
-    '/profile/:path*',
-    '/login',
-    '/signup',
-  ]
-}
+	matcher: ['/', '/profile', '/profile/:path*', '/login', '/signup', '/verification'],
+};
