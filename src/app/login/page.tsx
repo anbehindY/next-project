0ff2e0351a652e2950f6ad7cc9fb5f51db235564
anbehindY <yanminthwin@gmail.com>
2023-10-21 21:28:13 +0630

    Add reset password link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,6 +11,7 @@ export default () => {
 		email: '',
 		password: '',
 	});
+
 	const [loading, setLoading] = React.useState(false);
 	const [buttonDisabled, setButtonDisabled] = React.useState(true);
 	const onLogin = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -36,21 +37,24 @@ export default () => {
 			setButtonDisabled(true);
 		}
 	}
-	, [user]);
+		, [user]);
 
 	return (
 		<div className='flex flex-col items-center justify-start bg-yellow-500 h-screen pt-24 text-black'>
-			<h1 className='text-2xl font-bold mb-6'>{loading? 'Signing you in' : 'Welcome from Next project'}</h1>
+			<h1 className='text-2xl font-bold mb-6'>{loading ? 'Signing you in' : 'Welcome from Next project'}</h1>
 			<form onSubmit={onLogin} className='flex flex-col items-center'>
-			<label htmlFor="email" className='mr-auto pb-1'>Email</label>
-				<input id='email' placeholder='Enter email' type='email' value={user.email} className='mb-2 rounded-sm px-2 py-1' onChange={e => setUser({...user, email: e.target.value})}/>
+				<label htmlFor="email" className='mr-auto pb-1'>Email</label>
+				<input id='email' placeholder='Enter email' type='email' value={user.email} className='mb-2 rounded-sm px-2 py-1' onChange={e => setUser({ ...user, email: e.target.value })} />
 				<label htmlFor="password" className='mr-auto pb-1'>Password</label>
-				<input id='password' placeholder='Enter password' type='password' value={user.password} className='mb-2 rounded-sm px-2 py-1' onChange={e => setUser({...user, password: e.target.value})}/>
+				<input id='password' placeholder='Enter password' type='password' value={user.password} className='mb-2 rounded-sm px-2 py-1' onChange={e => setUser({ ...user, password: e.target.value })} />
 				<button type='submit' className='p-2 bg-sky-950 text-slate-300 rounded-md mt-4'>{buttonDisabled ? 'Not yet' : 'Login'}</button>
 			</form>
-      <Link href='/signup' className='text-indigo-950 mt-4'>
-        Doesn't have an account? Sign up here
-      </Link>
+			<Link href='/signup' className='text-indigo-950 mt-4'>
+				Doesn't have an account? Sign up here
+			</Link>
+			<Link href='/reset' className='text-indigo-950 mt-4'>
+				Forgot password? Reset here
+			</Link>
 		</div>
 	);
 };
