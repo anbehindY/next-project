791ae801d1730e17499333c87a8b076a4bd23042
anbehindY <yanminthwin@gmail.com>
2023-11-01 19:00:00 +0630

    testing react query

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,11 +4,33 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+  dataTagSymbol,
+} from '@tanstack/react-query'
 
+
+type User = {
+  _id?: string;
+  email?: string;
+  username?: string;
+  role?: string;
+}
 
 export default function Profile() {
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['userDetail'], queryFn: async () => {
+      const { data } = await axios.get('/api/users/userDetail')
+      return data.data as User;
+    }
+  });
+
   const router = useRouter();
-  const [userData, setUserData] = useState('nothing yet')
   const logout = async () => {
     try {
       await axios.get('/api/users/logout')
@@ -19,26 +41,26 @@ export default function Profile() {
     }
   }
 
-  useEffect(() => {
-    getUserData()
-  }, [userData])
-
-  const getUserData = async () => {
-    try {
-      const res = await axios.get('/api/users/userDetail')
-      setUserData(res.data.data._id)
-    } catch (error: any) {
-      console.log(error.message);
-      toast.error(error.message);
-    }
+  if (isError) {
+    return <div>Something went wrong</div>
   }
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-8">
       <h1 className="text-2xl">Profile</h1>
-      <p className="text-xl text-white">{userData !== 'nothing yet' ?
+      {/* <p className="text-xl text-white">{userData !== 'nothing yet' ?
         <Link href={`/profile/${userData}`}>{userData}</Link>
         : 'loading'}
-      </p>
+      </p> */}
+
+      {isLoading
+        ? <p className="text-xl text-white">Loading...</p>
+        :
+        <ul>
+          <li>User ID: <Link href={`/profile/${data?._id}`}>{data?._id}</Link></li>
+          <li>Username: {data?.username}</li>
+          <li>Email: {data?.email}</li>
+        </ul>
+      }
       <button
         className="bg-sky-500 p-2"
         onClick={logout}
